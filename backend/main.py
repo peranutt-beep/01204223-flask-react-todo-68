cc1ab2b29b9f55b7d9ad09b812f9e70e80baf4ea
Peranut Tasanavinyu <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>
2026-01-23 14:20:41 +0700

    Add InitMigration

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +16,7 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db)
 
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -26,9 +29,6 @@ class TodoItem(db.Model):
             "title": self.title,
             "done": self.done
         }
-
-with app.app_context():
-    db.create_all()
 
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),

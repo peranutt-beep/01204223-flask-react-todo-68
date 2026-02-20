b9da5392d4e908e66b5f5f47438690254b91f59d
Peranut Tasanavinyu <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>
2026-02-20 13:42:23 +0700

    Add some more test

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -1,5 +1,5 @@
 import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { expect } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
 
